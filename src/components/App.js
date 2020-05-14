3dac1ab7b69e82e7d1aed1c4bf305fc8fb750d0f
Joshua Forman <cyborgforman@gmail.com>
2020-05-13 20:12:24 -0700

    clean slate

--- src/components/App.js
+++ src/components/App.js
@@ -1,24 +1,14 @@
-import React, { useState } from 'react';
-import ReactDOM from "react-dom";
-import MyTree from './MyTree';
-// import YearForest from './YearForest';
-import HabitController from './HabitController';
-import Login from './Login';
+import React from 'react';
+import "./App.css"
 
 
 function App() {
 
-  const [habitControllerDisplay, setHabitControllerDisplay] = useState('none');
-  const [loginDisplay, setLoginDisplay] = useState('block');
 
   return (
     <div className='app'>
-      <div className='loginDisplay' style={{display: `${loginDisplay}`}}>
-        <Login setHabitControllerDisplay={setHabitControllerDisplay} setLoginDisplay={setLoginDisplay}/>
-      </div>
-
-      <div className='habitControllerDisplay' style={{display: `${habitControllerDisplay}`}}>
-        <HabitController />
+      <div>
+        hello world
       </div>
 
     </div>
