13fba672a0163acb32321b0e947c353322e2eaa8
Joshua Forman <cyborgforman@gmail.com>
2020-05-17 10:01:23 -0700

    add content and style to aboutMe

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -11,14 +11,33 @@ function AboutMe(){
         <div className='imgWrapperA'>
           <img className='mathImg' src={AksharDave}></img>
         </div>
-        <h3 className='love'>I am a front end developer having most recently worked with Reactjs, JS, and SCSS.</h3>
+        <h3 className='love'>I am a front end developer. Most recently I have been working with Reactjs, JS, and SCSS.</h3>
       </div>
 
       <div className='block'>
-        <h3 className='love'>I love, math, film, education, and programming.</h3>
+        <h3 className='love'>I love math, film, education, and programming.</h3>
         <div className='imgWrapperB'>
         <img className='mathImg' src={AksharDave}></img>
         </div>
+      </div>
+
+      <div className='block'>
+        <div className='imgWrapperA'>
+        </div>
+        <h3 className='love'>Recently I finished an internship at Theorem Agency where I created client deliverables for companies such as Aflac Insurance, the Portland Trailblazers, and the LA Galaxy. We worked to both create clean front end experiences, as well as process analytic reports for well informed business decisions.</h3>
+      </div>
+
+      <div className='block'>
+        <h3 className='love'>I was trained to program at Epicodus in Portland, Oregon. I hold a Masters in Mathematics Education from DePaul University and a BA in Philosophy from Wayne State University.</h3>
+        <div className='imgWrapperB'>
+        <img className='mathImg' src={AksharDave}></img>
+        </div>
+      </div>
+
+      <div className='block'>
+        <div className='imgWrapperA'>
+        </div>
+        <h3 className='love'>Before getting into programming, I taught mathematics, yoga, and film editing for 8 years.</h3>
       </div>
     </div>
   )
