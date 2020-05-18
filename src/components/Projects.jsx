965c961019e151f44b2a0f387850d52c6529d79b
Joshua Forman <cyborgforman@gmail.com>
2020-05-18 15:54:21 -0700

    add myForest to projects

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,11 +1,28 @@
 import React from 'react';
+import '../css/projects.css';
+import MyForest from '../img/myForest.png';
 
 
 function Projects(){
 
   return(
     <div>
-      sites and apps
+      <div className='titleWrapper'>
+        <h1 className='title'>Projects</h1>
+      </div>
+
+      <div className='block'>
+        <div className=''>
+          <a href='https://dry-forest-61437.herokuapp.com/'>
+            <img className='myForestImg' src={MyForest}></img>
+          </a>
+        </div>
+        <div className='wordWrapper'>
+          <h1 className='wordContent'>myForest</h1>
+          <h3 className='wordContent'>MyForest is an application built in react for the purpose of letting users tack and visually represent personal habits, events, and data.</h3>
+        </div>
+      </div>
+
     </div>
     )
   }
