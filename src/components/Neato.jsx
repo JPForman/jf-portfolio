34f7230bc12193155000f46a088df673e2fcdcc2
Joshua Forman <cyborgforman@gmail.com>
2021-05-06 22:07:54 -0600

    working neato buttons

--- src/components/Neato.jsx
+++ src/components/Neato.jsx
@@ -1,28 +1,38 @@
-import React from 'react'
+import React, { useState } from "react";
 
-import '../css/neato.css'
+import "../css/neato.css";
 
 const Neato = () => {
-  return (
-    <div className='neato'>
-      <div className='neato-block'></div>
-      <div className='neato-block'></div>
-      <div className='neato-block'></div>
-      <div className='neato-block'></div>
-      <div className='neato-block'></div>
-      <div className='neato-block'></div>
-      <div className='neato-block'></div>
-      <div className='neato-block'></div>
-      <div className='neato-block'></div>
-      <div className='neato-block'></div>
-      <div className='neato-block'></div>
-      <div className='neato-block'></div>
-      <div className='neato-block'></div>
-      <div className='neato-block'></div>
-      <div className='neato-block'></div>
-      <div className='neato-block'></div>
-    </div>
-  )
-}
+  const [backColor, setBackColor] = useState("61304B");
+  const [blockNumber, setBlockNumber] = useState(5);
 
-export default Neato
+  const coolColors = ["5C0029", "61304B", "857C8D", "94BFBE", "ACF7C1"];
+
+  const blocks = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16];
+
+  const blockClickHandler = () => {
+    const randBlock = Math.floor(Math.random() * 16) + 1  
+    const randColor = Math.floor(Math.random() * coolColors.length)  
+
+    setBackColor(coolColors[randColor]);
+    setBlockNumber(randBlock);
+  }
+
+  return (
+    <div className="neato">
+      {blocks.map((block) =>
+        blockNumber === block ? (
+          <div
+            className="neato-block"
+            style={{ backgroundColor: `#${backColor}` }}
+            onClick={blockClickHandler}
+          ></div>
+        ) : (
+          <div className="neato-block"></div>
+        )
+      )}
+    </div>
+  );
+};
+
+export default Neato;
