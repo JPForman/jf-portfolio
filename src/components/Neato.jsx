889cc8fe250a3d53a2862ab0fbd6ec518f848788
Joshua Forman <cyborgforman@gmail.com>
2021-05-06 22:27:19 -0600

    update neato colors

--- src/components/Neato.jsx
+++ src/components/Neato.jsx
@@ -6,7 +6,7 @@ const Neato = () => {
   const [backColor, setBackColor] = useState("61304B");
   const [blockNumber, setBlockNumber] = useState(5);
 
-  const coolColors = ["5C0029", "61304B", "857C8D", "94BFBE", "ACF7C1"];
+  const coolColors = ["5C0029", "61304B", "857C8D", "94BFBE", "ACF7C1", '785964', '82A7A6', '000000', '9ED0E6', 'B796AC'];
 
   const blocks = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16];
 
