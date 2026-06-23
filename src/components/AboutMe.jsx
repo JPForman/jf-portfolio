21ba85a101f596c189fa3d48a3c18488b4c36a48
Josh Forman <cyborgforman@gmail.com>
2026-06-22 21:16:37 -0700

    updates in content verbage

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -1,9 +1,11 @@
 import React from "react";
-import { Link } from "react-router-dom";
-import AksharDave from "../img/AksharDave.jpg";
 import MeMash from "../img/MeMash.png";
-import SriYantra from "../img/SriYantra.png";
-import Theorem from '../img/theorem.png'
+import Lucerna from "../img/lucerna.png";
+import FSLogo from "../img/fs_logo.svg";
+import Kalpa from "../img/kalpa.png";
+import Cynerge from "../img/cynerge.png";
+import ClassBucksFull from "../img/ClassbucksLogoFull.jpg";
+import Theorem from '../img/theorem.png';
 import LAGalaxy from "../img/LAGalaxy.png";
 import Trailblazers from "../img/Trailblazers.png";
 import Aflac from "../img/Aflac.png";
@@ -13,7 +15,6 @@ import WSU from "../img/wsu.png";
 import DATA from "../img/DATAlogo.jpg";
 import CTDHS from "../img/ctdhs.jpg";
 import Editing from "../img/editing.jpg";
-import ClassBucksLogo from "../img/ClassBucksLogo.jpg";
 import Footer from "./Footer";
 
 import "../css/aboutMe.css";
@@ -28,76 +29,96 @@ const AboutMe = ({ darkmode }) => {
       <div className="block">
         <div className="word-wrapper">
           <span className="word-content">
-          I am the lead developer at Class Bucks Llc., a company committed to supporting teachers with a classroom management and content delivery educational web app. My daily stack includes JavaScript, React, Firebase, and SCSS. I work with great people and love what I do.
+            I am a full-stack developer with 6+ years building production web and mobile applications across education, government, and startup environments. Currently I'm an Application Developer at Lucerna Digital, where I build customized client applications using React, Google Firebase, and PostgreSQL with a focus on clean architecture and rapid iteration.
           </span>
         </div>
         <div className="img-wrapper">
-          <img className="project-img" src={MeMash}></img>
+          <img className="project-img" src={MeMash} alt="Joshua Forman" />
         </div>
       </div>
 
       <div className="block">
         <div className="word-wrapper">
           <span className="word-content">
-          I enjoy math, learning, communication, and expanding my programming horizons. While I'm not coding at ClassBucks, I sometimes teach programming and yoga classes at DATA Albuquerque Charter School.
+            At Lucerna Digital I design and maintain automated CI/CD pipelines, apply AI-assisted development tools including Claude Code and Cursor to accelerate feature delivery, and contribute to product strategy and customer outreach as well as the technical side of things.
           </span>
         </div>
-
         <div className="img-wrapper">
-          <div className="link-row">
-            <a href="https://classbucks.com/" className="">
-              <img className="row-img" src={ClassBucksLogo}></img>
-            </a>
-            <a href="http://datacharter.org/" className="">
-              <img className="row-img" src={DATA}></img>
-            </a>
-          </div>
+          <img className={`project-img${!darkmode ? ' project-img-invert' : ''}`} src={Lucerna} alt="Lucerna Digital logo" />
         </div>
       </div>
 
       <div className="block">
         <div className="word-wrapper">
           <span className="word-content">
-            Before working at ClassBucks, I was a front-end developer intern at Theorem Agency where I created client deliverables for companies such as Aflac Insurance, the Portland Trailblazers, and the LA Galaxy. We worked to both create clean front end experiences, as well as process analytic reports for well informed business decisions.
+            Before Lucerna, I was an Application Developer at Cynerge Consulting. There I developed the Angular front-end for a federal mission-tracking system as part of the U.S. Forest Service and BLM Smokejumper modernization effort, working within a secure VPN environment with federal security clearance.
+          </span>
+        </div>
+        <div className="img-wrapper">
+          <img className="project-img" src={FSLogo} alt="U.S. Forest Service logo" />
+        </div>
+      </div>
+
+      <div className="block">
+        <div className="word-wrapper">
+          <span className="word-content">
+            Also at Cynerge, I led Angular front-end development of Kalpa v4, a professional development reporting system, from inception. I defined the component architecture, built data visualization features with ChartJS, and implemented AWS Cognito authentication with federated social sign-in. Stack: Angular, Angular Material, ChartJS, AWS Amplify, Node.js, Express, Sequelize, PostgreSQL, AWS Aurora.
+          </span>
+        </div>
+        <div className="img-wrapper">
+          <a href="https://kalpasolutions.com/">
+            <img className="project-img" src={Kalpa} alt="Kalpa Solutions logo" />
+          </a>
+        </div>
+      </div>
+
+      <div className="block">
+        <div className="word-wrapper">
+          <span className="word-content">
+            Prior to Cynerge, I was the Lead Application Developer at Class Bucks LLC, where I built a gamified classroom management platform from the ground up as sole lead developer, taking it from concept to production. Stack: React, Bootstrap, Google Firebase.
+          </span>
+        </div>
+        <div className="img-wrapper">
+          <a href="https://classbucks.com/">
+            <img className="project-img" src={ClassBucksFull} alt="ClassBucks logo" />
+          </a>
+        </div>
+      </div>
+
+      <div className="block">
+        <div className="word-wrapper">
+          <span className="word-content">
+            I got my start as a front-end developer intern at Theorem Agency, where I delivered React components for enterprise clients including Aflac Insurance, the Portland Trail Blazers, and the LA Galaxy. We worked within an agile team to meet tight client deadlines with polished, responsive UI.
           </span>
         </div>
         <div className="img-wrapper">
           <a href="https://theoreminc.net/" className={!darkmode ? "theorem" : 'theorem-dark'}>
-            <img className="project-img" src={Theorem}></img>
+            <img className="project-img" src={Theorem} alt="Theorem Agency logo" />
           </a>
           <div className="theorem-group">
-            <img className="" src={LAGalaxy}></img>
-            <img className="" src={Trailblazers}></img>
-            <img className="" src={Aflac}></img>
+            <img src={LAGalaxy} alt="LA Galaxy" />
+            <img src={Trailblazers} alt="Portland Trail Blazers" />
+            <img src={Aflac} alt="Aflac" />
           </div>
         </div>
       </div>
 
       <div className="block">
-        <span className="word-content">
-          Programming Is Cool!
-        </span>
-      </div>
-      <div></div>
-
-      <div className="block">
         <div className="word-wrapper">
           <span className="word-content">
-            I was trained to program at Epicodus in Portland, Oregon. I hold a
-            Masters in Mathematics Education from DePaul University and a BA in
-            Philosophy from Wayne State University.
+            I was trained to program at Epicodus in Portland, Oregon. I hold a Master of Arts in Mathematics Education from DePaul University and a Bachelor of Arts in Philosophy from Wayne State University.
           </span>
         </div>
         <div className="img-wrapper">
           <div className="link-row">
-            <a href="https://www.depaul.edu/Pages/default.aspx">
-              <img className="row-img" src={DePaul}></img>
-            </a>
             <a href="https://www.epicodus.com/">
-              <img className="row-img" src={Epicodus}></img>
+              <img className="row-img" src={Epicodus} alt="Epicodus" />
+            </a>
+            <a href="https://www.depaul.edu/Pages/default.aspx">
+              <img className="row-img" src={DePaul} alt="DePaul University" />
             </a>
             <a href="https://wayne.edu/">
-              <img className="row-img" src={WSU}></img>
+              <img className="row-img" src={WSU} alt="Wayne State University" />
             </a>
           </div>
         </div>
@@ -106,16 +127,15 @@ const AboutMe = ({ darkmode }) => {
       <div className="block">
         <div className="word-wrapper">
           <span className="word-content">
-            Before getting into programming, I taught mathematics, yoga, and film
-            editing for 8 years.
+            Before getting into programming, I taught mathematics, yoga, and film editing.
           </span>
         </div>
         <div className="img-wrapper">
           <div className="link-row">
             <a href="http://datacharter.org/">
-              <img className="row-img" src={DATA}></img>
+              <img className="row-img" src={DATA} alt="DATA Albuquerque Charter School" />
             </a>
-            <img className="row-img" src={CTDHS}></img>
+            <img className="row-img" src={CTDHS} alt="CTDHS" />
           </div>
         </div>
       </div>
@@ -127,7 +147,7 @@ const AboutMe = ({ darkmode }) => {
           </span>
         </div>
         <div className="img-wrapper">
-          <img className="editing" src={Editing}></img>
+          <img className="editing" src={Editing} alt="video editing" />
         </div>
       </div>
 
